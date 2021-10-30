fd7dfd83b74cda91b1739bf96b30bbe5057a43ed
Miraculous Owonubi <omiraculous@gmail.com>
2021-10-30 23:52:57 +0100

    introduce the group and exclude workspace meta opts

--- cargo-workspaces/src/utils/config.rs
+++ cargo-workspaces/src/utils/config.rs
@@ -1,6 +1,7 @@
-use crate::utils::{Error, Result};
+use crate::utils;
 
-use serde::Deserialize;
+use glob::Pattern;
+use serde::{de, Deserialize};
 use serde_json::{from_value, Value};
 
 #[derive(Deserialize, Default)]
@@ -9,12 +10,12 @@ struct MetadataWorkspaces<T> {
 }
 
 // TODO: Validation of conflicting options (hard to tell conflicts if between cli and option)
-pub fn read_config<T>(value: &Value) -> Result<T>
+pub fn read_config<T>(value: &Value) -> utils::Result<T>
 where
     T: for<'de> Deserialize<'de> + Default,
 {
     from_value::<Option<MetadataWorkspaces<T>>>(value.clone())
-        .map_err(Error::BadMetadata)
+        .map_err(utils::Error::BadMetadata)
         .map(|v| v.unwrap_or_default().workspaces.unwrap_or_default())
 }
 
@@ -23,8 +24,35 @@ pub struct PackageConfig {
     pub independent: Option<bool>,
 }
 
+#[derive(Deserialize, Debug, Clone, Ord, Eq, PartialOrd, PartialEq)]
+pub struct PackageGroup {
+    pub name: String,
+    #[serde(deserialize_with = "deserialize_members")]
+    pub members: Vec<Pattern>,
+}
+
+#[derive(Deserialize, Debug, Clone, Ord, Eq, PartialOrd, PartialEq)]
+#[serde(transparent)]
+pub struct ExcludeSpec {
+    #[serde(deserialize_with = "deserialize_members")]
+    pub members: Vec<Pattern>,
+}
+
 #[derive(Deserialize, Default, Debug, Clone, Ord, Eq, PartialOrd, PartialEq)]
 pub struct WorkspaceConfig {
+    pub exclude: Option<ExcludeSpec>,
+    pub group: Option<Vec<PackageGroup>>,
     pub allow_branch: Option<String>,
     pub no_individual_tags: Option<bool>,
+}
+
+fn deserialize_members<'de, D>(deserializer: D) -> Result<Vec<Pattern>, D::Error>
+where
+    D: de::Deserializer<'de>,
+{
+    Vec::<String>::deserialize(deserializer)?
+        .into_iter()
+        .map(|s| Pattern::new(&s))
+        .collect::<Result<_, _>>()
+        .map_err(de::Error::custom)
 }
